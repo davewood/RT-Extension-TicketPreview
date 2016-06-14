b1ce6682656dcc0cffb41306caefccf89eafe860
David Schmidt <mail@davidschmidt.at>
2016-06-14 09:39:15 +0200

    use first text/plain attachment

--- lib/RT/Ticket_Local.pm
+++ lib/RT/Ticket_Local.pm
@@ -2,25 +2,37 @@ package RT::Ticket;
 
 use strict;
 use warnings;
+use Encode qw/decode_utf8/;
 
 sub TicketPreview {
-    my $self       = shift;
-    my $num_lines  = RT->Config->Get('TicketPreview')->{num_lines};
-    my $res        = $self->Attachments();
-    my $attachment = $res->First;
+    my $self        = shift;
+    my $attachment = $self->_get_first_plain_att;
     return '' unless $attachment;
     my $content = $attachment->Content;
     return '' unless $content;
+    return _get_preview($content);
+}
+
+sub _get_preview {
+    my ($content) = @_;
+    my $num_lines  = RT->Config->Get('TicketPreview')->{num_lines};
     open( my $fh, '<', \$content ) or die $!;
-    my @preview = ();
+    my @lines = ();
     while ( my $line = <$fh> ) {
-        # rm html tags
-        $line =~ s/\<.*?\>//xmsg;
-        # ignore empty lines
-        push( @preview, $line ) if ( $line !~ m/^\s*$/xms );
-        last if scalar @preview >= $num_lines;
+        next if $line =~ m/^\s*$/xms; # ignore empty lines
+        push( @lines, $line );
+        last if scalar @lines >= $num_lines;
     }
-    return join('', @preview);
+    my $preview = join('', @lines);
+    return decode_utf8($preview);
+}
+
+sub _get_first_plain_att {
+    my $self        = shift;
+    my $attachments = $self->Attachments();
+    while ( my $att = $attachments->Next ) {
+        return $att if $att->ContentType eq 'text/plain';
+    }
 }
 
 1;
